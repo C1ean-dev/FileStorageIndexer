1f4f96bc766528ef09190475446b4ff7563bfbbe
clean <erick13r13@gmail.com>
2025-07-25 15:56:08 -0300

    abrir pasta quando tiver apenas um resultado

--- modules/search_folder.py
+++ modules/search_folder.py
@@ -1,3 +1,4 @@
+import os
 from core.indexer import FileIndexer
 
 def search_folder_menu(indexer: FileIndexer):
@@ -6,10 +7,15 @@ def search_folder_menu(indexer: FileIndexer):
     if search_term:
         results = indexer.search_folders(search_term)
         if results:
-            print(f"\nEncontradas {len(results)} pasta(s):")
-            for folder_name, full_path, parent_path in results:
-                print(f"\nPasta: {folder_name}")
-                print(f"Caminho: \"{full_path}\"")
-                print(f"Pasta Pai: {parent_path}")
+            if len(results) == 1:
+                folder_name, full_path, parent_path = results[0]
+                print(f"Abrindo pasta encontrada: \"{full_path}\"")
+                os.startfile(full_path)
+            else:
+                print(f"\nEncontradas {len(results)} pasta(s):")
+                for folder_name, full_path, parent_path in results:
+                    print(f"\nPasta: {folder_name}")
+                    print(f"Caminho: \"{full_path}\"")
+                    print(f"Pasta Pai: {parent_path}")
         else:
             print("Nenhuma pasta encontrada.")
