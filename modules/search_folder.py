8e703c8fbd145423553892bf2bcda0138dc8ab4d
clean <erick13r13@gmail.com>
2025-07-25 15:05:46 -0300

    modified:   modules/search_folder.py

--- modules/search_folder.py
+++ modules/search_folder.py
@@ -9,7 +9,7 @@ def search_folder_menu(indexer: FileIndexer):
             print(f"\nEncontradas {len(results)} pasta(s):")
             for folder_name, full_path, parent_path in results:
                 print(f"\nPasta: {folder_name}")
-                print(f"Caminho: {full_path}")
+                print(f"Caminho: \"{full_path}\"")
                 print(f"Pasta Pai: {parent_path}")
         else:
             print("Nenhuma pasta encontrada.")
